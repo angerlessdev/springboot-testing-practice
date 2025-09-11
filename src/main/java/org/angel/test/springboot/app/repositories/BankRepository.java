66af40fa1d907446803046c9cab725bae66b1f7e
Angel Cancho <u201721995@upc.edu.pe>
2025-09-10 19:39:05 -0500

    feat(repositories): add BankRepository interface

---
+++ src/main/java/org/angel/test/springboot/app/repositories/BankRepository.java
@@ -0,0 +1,11 @@
+﻿package org.angel.test.springboot.app.repositories;
+
+import org.angel.test.springboot.app.models.Bank;
+
+import java.util.List;
+
+public interface BankRepository {
+    List<Bank> findAll();
+    Bank findById(Long id);
+    void update(Bank bank);
+}
