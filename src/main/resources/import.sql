a76b6de1bacf9574078c426981fe380a1cf0f5d1
Angel Cancho <u201721995@upc.edu.pe>
2025-09-11 18:36:49 -0500

    chore(db): add import.sql for initial data seeding

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+﻿INSERT INTO accounts (person, balance) VALUES ('Angel', 1000);
+INSERT INTO accounts (person, balance) VALUES ('Estrella', 2000);
+INSERT INTO banks (name, total_transfers) VALUES ('The financial bank', 0);
