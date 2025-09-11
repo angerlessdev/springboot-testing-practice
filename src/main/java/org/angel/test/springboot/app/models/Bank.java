c1264aa0a3ce1f0572863de28279ff2f2b1aec67
Angel Cancho <u201721995@upc.edu.pe>
2025-09-10 19:35:02 -0500

    feat(models): add Bank entity

---
+++ src/main/java/org/angel/test/springboot/app/models/Bank.java
@@ -0,0 +1,40 @@
+﻿package org.angel.test.springboot.app.models;
+
+public class Bank {
+    private Long id;
+    private String name;
+    private Integer totalTransactions;
+
+    public Bank() {
+    }
+
+    public Bank(Long id, String name, Integer totalTransactions) {
+        this.id = id;
+        this.name = name;
+        this.totalTransactions = totalTransactions;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getTotalTransactions() {
+        return totalTransactions;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTotalTransactions(Integer totalTransactions) {
+        this.totalTransactions = totalTransactions;
+    }
+}
