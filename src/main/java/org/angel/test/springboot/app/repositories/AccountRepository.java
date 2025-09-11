5299ce699d6d0c9e975f14fb7436477420a0278d
Angel Cancho <u201721995@upc.edu.pe>
2025-09-10 19:37:56 -0500

    feat(repositories): add AccountRepository interface

---
+++ src/main/java/org/angel/test/springboot/app/repositories/AccountRepository.java
@@ -0,0 +1,11 @@
+﻿package org.angel.test.springboot.app.repositories;
+
+import org.angel.test.springboot.app.models.Account;
+
+import java.util.List;
+
+public interface AccountRepository {
+    List<Account> findAll();
+    Account findById(Long id);
+    void update(Account account);
+}
