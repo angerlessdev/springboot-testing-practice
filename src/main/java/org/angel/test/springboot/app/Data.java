da277a972e80618c380258829cedfafd5405355e
Angel Cancho <u201721995@upc.edu.pe>
2025-09-11 02:35:32 -0500

    test(data): add predefined Account and Bank test data

---
+++ src/main/java/org/angel/test/springboot/app/Data.java
@@ -0,0 +1,12 @@
+﻿package org.angel.test.springboot.app;
+
+import org.angel.test.springboot.app.models.Account;
+import org.angel.test.springboot.app.models.Bank;
+
+import java.math.BigDecimal;
+
+public class Data {
+    public static final Account ACCOUNT_001 = new Account(1L, "Angel", new BigDecimal("1000"));
+    public static final Account ACCOUNT_002 = new Account(2L, "Estrella", new BigDecimal("2000"));
+    public static final Bank BANK = new Bank(1L, "The financial bank", 0);
+}
