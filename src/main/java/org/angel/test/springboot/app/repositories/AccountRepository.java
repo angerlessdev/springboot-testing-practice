20147431fc2296060291dbcb2d74e35fa4343a5a
Angel Cancho <u201721995@upc.edu.pe>
2025-09-11 19:24:33 -0500

    refactor(repository): migrate AccountRepository to JpaRepository

--- src/main/java/org/angel/test/springboot/app/repositories/AccountRepository.java
+++ src/main/java/org/angel/test/springboot/app/repositories/AccountRepository.java
@@ -1,12 +1,13 @@
 package org.angel.test.springboot.app.repositories;
 
 import org.angel.test.springboot.app.models.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface AccountRepository {
-    List<Account> findAll();
-    Account findById(Long id);
-    void update(Account account);
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    // List<Account> findAll();
+    // Account findById(Long id);
+    // void update(Account account);
 }
