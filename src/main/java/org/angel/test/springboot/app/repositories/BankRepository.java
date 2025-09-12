c6763eece2bb19a7e43536cafd7af5998b5c3ba7
Angel Cancho <u201721995@upc.edu.pe>
2025-09-11 19:24:48 -0500

    refactor(repository): migrate BankRepository to JpaRepository

--- src/main/java/org/angel/test/springboot/app/repositories/BankRepository.java
+++ src/main/java/org/angel/test/springboot/app/repositories/BankRepository.java
@@ -1,12 +1,13 @@
 package org.angel.test.springboot.app.repositories;
 
 import org.angel.test.springboot.app.models.Bank;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface BankRepository {
-    List<Bank> findAll();
-    Bank findById(Long id);
-    void update(Bank bank);
+public interface BankRepository extends JpaRepository<Bank, Long> {
+    // List<Bank> findAll();
+    // Bank findById(Long id);
+    // void update(Bank bank);
 }
