bf04d2a0d631488a828920dbfe0a6cca28eea701
Angel Cancho <u201721995@upc.edu.pe>
2025-09-15 22:30:03 -0500

    feat(controller): add AccountController with account detail and transfer endpoints

---
+++ src/main/java/org/angel/test/springboot/app/controllers/AccountController.java
@@ -0,0 +1,44 @@
+﻿package org.angel.test.springboot.app.controllers;
+
+import org.angel.test.springboot.app.models.Account;
+import org.angel.test.springboot.app.models.TransferDto;
+import org.angel.test.springboot.app.services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Controller
+@RequestMapping("/api/accounts")
+public class AccountController {
+    @Autowired
+    private AccountService accountService;
+    @GetMapping("/{id}")
+    @ResponseStatus(OK)
+    public Account detail(@PathVariable(name="id") Long id) {
+        return accountService.findAccountById(id);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<?> transfer(@RequestBody TransferDto transferDto) {
+        accountService.transfer(
+                transferDto.getAccountSourceId(),
+                transferDto.getAccountDestinationId(),
+                transferDto.getAmount(),
+                transferDto.getBankId());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", OK);
+        response.put("message", "Transfer successful");
+        response.put("transfer", transferDto);
+
+        return ResponseEntity.ok(response);
+    }
+}
