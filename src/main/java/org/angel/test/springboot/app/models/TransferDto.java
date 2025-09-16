e5b824da60861e6bd0c18c8681adba5ebb83544d
Angel Cancho <u201721995@upc.edu.pe>
2025-09-15 22:29:13 -0500

    feat(dto): add TransferDto for handling account transfers

---
+++ src/main/java/org/angel/test/springboot/app/models/TransferDto.java
@@ -0,0 +1,42 @@
+﻿package org.angel.test.springboot.app.models;
+
+import java.math.BigDecimal;
+
+public class TransferDto {
+    private Long accountSourceId;
+    private Long accountDestinationId;
+    private BigDecimal amount;
+    private Long bankId;
+
+    public Long getBankId() {
+        return bankId;
+    }
+
+    public void setBankId(Long bankId) {
+        this.bankId = bankId;
+    }
+
+    public Long getAccountSourceId() {
+        return accountSourceId;
+    }
+
+    public void setAccountSourceId(Long accountSourceId) {
+        this.accountSourceId = accountSourceId;
+    }
+
+    public Long getAccountDestinationId() {
+        return accountDestinationId;
+    }
+
+    public void setAccountDestinationId(Long accountDestinationId) {
+        this.accountDestinationId = accountDestinationId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
